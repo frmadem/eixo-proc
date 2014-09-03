55e58a1f1a9f20d9c59593f2f774ee877d60a142
J. Gomez <alambike@gmail.com>
2014-09-03 18:09:42 +0200

    Bump version to 0.020 Create Eixo::Proc module api methods (ps, search), with a ps cache in package

--- lib/Eixo/Proc.pm
+++ lib/Eixo/Proc.pm
@@ -3,9 +3,28 @@ package Eixo::Proc;
 use 5.008;
 use strict;
 use warnings;
+use Eixo::Proc::Ps;
 
 
+our $VERSION = '0.020';
 
-our $VERSION = '0.01';
+our $ps = undef;
+
+sub ps {
+
+	return $ps->actualizar if($ps);
+
+	$ps = Eixo::Proc::Ps->new->parsear();
+
+}
+
+sub search{
+	shift if(@_ % 2);
+
+	
+	Eixo::Proc->ps->search(@_);	
+
+}
 
 
+1;
